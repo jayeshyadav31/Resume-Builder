a02ad1b289e0c2e6ba9b303df6731a6ab0cb9f5d
jayeshyadav31 <104785686+jayeshyadav31@users.noreply.github.com>
2024-05-05 14:41:31 +0530

    some changes

--- frontend/src/congif/firebase.js
+++ frontend/src/congif/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth"; // Import specifically for auth
 import firebase from 'firebase/compat/app'; // Use 'compat' for older syntax compatibility
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPW3A-_SkjKand6PCe8OPxMrrlKrpEKp4",
-  authDomain: "resume-d1573.firebaseapp.com",
-  projectId: "resume-d1573",
-  storageBucket: "resume-d1573.appspot.com",
-  messagingSenderId: "795683378301",
-  appId: "1:795683378301:web:2f989e9eba13d7efa74d14",
-  measurementId: "G-SJC1ST1P98"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
